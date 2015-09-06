7b0f11590ac0a7ec915a3e7a812e58b917375139
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 14:11:46 +0200

    Update AVA

--- test.js
+++ test.js
@@ -6,12 +6,12 @@ test(function (t) {
 	t.plan(4);
 
 	repoExists('arthurvr/something-that-does-not-exist', function (error, exists) {
-		t.assert(!error, error);
-		t.assert(!exists);
+		t.error(error);
+		t.false(exists);
 	});
 
 	repoExists('h5bp/html5-boilerplate', function (error, exists) {
-		t.assert(!error, error);
-		t.assert(exists);
+		t.error(error);
+		t.true(exists);
 	});
 });
